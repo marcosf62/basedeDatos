da9ba067575870aa8a2b2403330589a921fc3a5e
marcosf62 <m.funes@itecriocuarto>
2025-09-22 20:25:18 -0300

    agrego script 5

--- Script-1.sql
+++ Script-1.sql
@@ -6,6 +6,19 @@ VALUES (NOW(),60, 2, 3, 'ACTIVA' );
 SELECT @id_max_ventas := MAX(id)FROM ventas ;
 
 -- Un script que inserte registros en la tabla detalle_factura usando el id obtenido en el script anterior.
-INSERT INTO detalle_factura (id_venta, id_producto, cantidad, precio)
-VALUES
-(@id_max_ventas, 1, 2, 150.00);
+INSERT INTO detalle_factura (id_ventas, id_producto, cantidad, precio_unitario)
+VALUES (@id_max_ventas, 8, 2, 30);
+
+-- Un script que obtenga la suma*producto de las cantidades y precios de los registros insertados en el punto anterior.
+SELECT SUM(cantidad * precio_unitario) AS monto_total
+FROM detalle_factura
+WHERE id_ventas  = @id_max_ventas; 
+
+-- Un script que actualice la tabla factura campo (Total) con el monto obtenido del script anterior
+UPDATE ventas v 
+SET total = (
+    SELECT SUM(cantidad * precio_unitario ) 
+    FROM detalle_factura 
+    WHERE id_ventas  = @id_max_factura
+)
+WHERE id = @id_max_factura;
