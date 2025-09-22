0b45324114dd720429d6ffd9fe170afabd8f1c94
marcosf62 <m.funes@itecriocuarto.org.ar>
2025-09-22 12:04:06 -0300

    agregando scrip con sqlyog

--- Script-1.sql
+++ Script-1.sql
@@ -1,6 +1,11 @@
 -- Un scipt que inserte un registro en la tabla factura
-INSERT INTO detalle_factura (id_ventas, id_producto, cantidad, precio_unitario)
-VALUES (6, 4, 8, 450.75)
+INSERT INTO ventas (fecha, total, id_persona, id_empleado, estado)
+VALUES (NOW(),60, 2, 3, 'ACTIVA' );
 
 -- Un scrpt que obtenga el id Máximo O ultimo de la tabla factura
-SELECT @id := MAX(id)FROM detalle_factura df ;
+SELECT @id_max_ventas := MAX(id)FROM ventas ;
+
+-- Un script que inserte registros en la tabla detalle_factura usando el id obtenido en el script anterior.
+INSERT INTO detalle_factura (id_venta, id_producto, cantidad, precio)
+VALUES
+(@id_max_ventas, 1, 2, 150.00);
